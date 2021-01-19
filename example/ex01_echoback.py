3222dbb5e5c2219e7bd0a28e2fec5e7ea8f5dbf3
Taiyou Komazawa <e1611216@toyama.kosen-ac.jp>
2021-01-20 02:55:44 +0900

    exampleの変更

--- example/ex01_echoback.py
+++ example/ex01_echoback.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+#encoding:utf-8
 
 import time
 import sys
@@ -9,15 +10,16 @@ import src.serial_bridge as pb
 
 vect1 = msg.Message("./vector3.yml")
 vect2 = msg.Message("./vector3.yml")
-dev = pb.SerialBridge("TestVect3", 16, "ttyS")
+dev = pb.SerialBridge("TestVect3", 16, "ttyUSB")
 
 dev.add_frame(vect2)
 
 while(True):
-    vect1.set(x= 0.321, y = 0.654, z = 0.123)
+    vect1.set(x= 0.321, y = 0.654, z = 0.123) #適当なデータ
     dev.send(0, vect1)
-    print(dev.recv())
-    print(vect2.data.x)
-    print(vect2.data.y)
-    print(vect2.data.z)
+    if dev.recv() == 0:
+        print(vect2.data.x)
+        print(vect2.data.y)
+        print(vect2.data.z)
+        print(" ")
     time.sleep(0.020)
